ac5ba6230a8a62317f3854d34374a9b479b49028
DavyReactiv <davycteam@gmail.com>
2026-08-04 15:57:31 +0200

    Clarify seasonal licences clubs and sporting levels

--- ufsc-clubs-licences-sql.php
+++ ufsc-clubs-licences-sql.php
@@ -78,6 +78,7 @@ require_once UFSC_CL_DIR.'inc/common/season.php';
 require_once UFSC_CL_DIR.'inc/common/seasons.php';
 require_once UFSC_CL_DIR.'inc/common/feature-flags.php';
 require_once UFSC_CL_DIR.'inc/common/licence-status.php';
+require_once UFSC_CL_DIR.'inc/common/fighter-level.php';
 require_once UFSC_CL_DIR.'inc/common/licence-documents.php';
 require_once UFSC_CL_DIR.'inc/common/attestations.php';
 require_once UFSC_CL_DIR.'inc/common/tables.php';
